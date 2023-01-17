a096f8895706c4b5a7210fe89190c5103666de28
gominsz <matheuscamargodealencar@gmail.com>
2023-01-16 23:50:26 -0300

    Debug_Css

---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+
+// async function get (url, params, headers) {
+// 	try {
+// 		console.log('[Request configs]:', url, params, headers);
+// 		const { status, data } = await axios.get('http://localhost:3333/pokemon/list');
+// 		if (status !== 200 ) throw new Error('Request error');
+// 		console.log(data);
+// 		return data;
+// 	} catch (error) {
+// 		throw new Error(error);
+// 	}
+// }
+
+function log() {
+  console.log("[Batatinha]");
+}
---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const app = express();
+
+app.use(express.static('public'));
+
+//habilitar o uso do req.body
+app.use(express.urlencoded({ extended: true }));
+
+nunjucks.configure('src/views', {
+	express: app,
+	noCache: true
+});
+
+app.get('/', (req, res) => {
+	res.render('index.html');
+	// res.json({ message: 'ok' });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log('🔥 App running on host: http://localhost:3000 🔥'));
